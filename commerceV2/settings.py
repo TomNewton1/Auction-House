7e98fb1cd524857ef1db5cb88de7841dde5ac204
Thomas Newton <tomnewtonx@gmail.com>
2021-06-15 11:49:43 +0100

    Staticfiles Base Dir

--- commerceV2/settings.py
+++ commerceV2/settings.py
@@ -133,11 +133,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-"""
+
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
-"""
 
 MEDIA_URL = '/images/'
 
