bc7437a3df04c61f479a210b14a39a62d4b7bb79
Thomas Newton <tomnewtonx@gmail.com>
2021-06-15 10:52:37 +0100

    Added database connection

--- commerceV2/settings.py
+++ commerceV2/settings.py
@@ -84,11 +84,12 @@ DATABASES = {
 }
 
 # Link to Heroku PostgresDB
-"""
+
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-"""
+
 
 AUTH_USER_MODEL = 'auctions.User'
 
